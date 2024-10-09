d29b143d20dc9ae72feadda4799e364bfe26e728
Junaidi Rahmat <junaidirahmat02@gmail.com>
2024-10-09 19:33:27 +0700

    feat : make topic API and configure prisma ORM

---
+++ routes/topicRoutes.js
@@ -0,0 +1,18 @@
+const {
+  createTopic,
+  getTopics,
+  getTopicById,
+} = require("../controller/topicController.js");
+
+const discussion = () => {
+  const router = express.Router();
+  router.post("/topic", createTopic);
+  router.get("/topics", getTopics);
+  router.get("/topic/:id", getTopicById);
+
+  return router;
+};
+
+module.exports = {
+  discussion,
+};
